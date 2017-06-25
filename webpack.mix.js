34f43617370640b61945ac2c84643340d5618598
Imam Susanto <abc.imams@gmail.com>
2017-06-25 22:45:05 +0700

    Update Dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,18 @@
 const { mix } = require('laravel-mix')
-const path = require('path')
 const WorkboxPlugin = require('workbox-webpack-plugin')
 
-mix.js('resources/js/app.js', 'static/js')
-   .stylus('resources/stylus/app.styl', 'static/css')
+mix.js('resources/js/app.js', 'public/js')
+   .stylus('resources/stylus/app.styl', 'public/css')
    .extract(['barba.js'])
    .webpackConfig({
      plugins: [
        new WorkboxPlugin({
-         globDirectory: './public/',
-         staticFileGlobs: [
+         globDirectory: 'public/',
+         globPatterns: [
            'css/**/*.css',
            'js/**/*.js'
          ],
-         swDest: path.join('./public/', 'service-worker.js')
+         swDest: 'public/service-worker.js'
        })
      ]
    })
